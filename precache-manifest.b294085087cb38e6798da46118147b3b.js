de64934e2c5a2d1978e498e58e5e6d0cf93848e7
Milan Janjevic <milan.janjevic@quby.com>
2019-05-13 22:07:35 +0200

    Updates

---
+++ precache-manifest.b294085087cb38e6798da46118147b3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5324934f50e8accfe8be4bbbb216c5f1",
+    "url": "/videorama/index.html"
+  },
+  {
+    "revision": "8401ad8dc70e3ec10723",
+    "url": "/videorama/static/css/main.b5dd9db4.chunk.css"
+  },
+  {
+    "revision": "72d78a9d52192b4cf685",
+    "url": "/videorama/static/js/2.1c1faf2c.chunk.js"
+  },
+  {
+    "revision": "8401ad8dc70e3ec10723",
+    "url": "/videorama/static/js/main.9407c78c.chunk.js"
+  },
+  {
+    "revision": "20c47304f659a5396bfb",
+    "url": "/videorama/static/js/runtime~main.2d14d5d5.js"
+  }
+]);
